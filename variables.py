6719a634fe5bf64e4aba90ea42889f8d6c7627e6
Nagaraju <nagarajun635@gmail.com>
2024-03-16 10:13:51 +0530

    added variables file

---
+++ variables.py
@@ -0,0 +1,60 @@
+# x = 5
+# y = 'nagaraju'
+#
+# print(type(x), type(y))
+
+# x = str(3)
+# y = int(3)
+# z = float(3)
+# print(x, y, z)
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# x = 5
+# y = "John"
+# print(x, y)
+
+# x = "awesome"
+#
+# def myfunc():
+#   print("Python is " + x)
+#
+# myfunc()
+
+# x = "awesome"
+#
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+#
+# myfunc()
+#
+# print("Python is " + x)
+
+# def myfunc():
+#   global x
+#   x = "fantastic"
+#
+# myfunc()
+#
+# print("Python is " + x)
+
+# x = "awesome"
+#
+# def myfunc():
+#   global x
+#   x = "fantastic"
+#
+# myfunc()
+#
+# print("Python is " + x)
+

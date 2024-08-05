732513fad88b45ac2adf470d634cc594a7696470
Nagaraju <nagarajun635@gmail.com>
2024-08-05 20:57:54 +0530

    added python formatting

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import sys
 
 print(sys.version)
+print('nama is {0} age is {1}'.format('naga',27))
+print('nama is %s age is %d'%('naga',27))
+print('%.9f'%(123456789.123456789))
+
+name='naga'
+print(f'name is {name}')

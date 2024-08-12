b7db098e8c0e99545c262680c87283576df748cc
Nagaraju <nagarajun635@gmail.com>
2024-08-12 13:57:25 +0530

    added file for getting cost to converting s1 into s2

---
+++ challenges/s1_to_s2_conv.py
@@ -0,0 +1,12 @@
+s1 = list('arjun')
+s2 = list('bindu')
+s3 = set(s2)
+print(s1,s2,s3)
+cst = 0
+for i in s3:
+    if i in s1:
+        if s2.count(i) > s1.count(i):
+            cst += s2.count(i)
+    else:
+        cst += 1
+print(cst)

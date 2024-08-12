f8c0b75d12d495925e6594eb24164c640fdf1a33
Nagaraju <nagarajun635@gmail.com>
2024-08-12 13:46:10 +0530

    modified test file with different decimal points

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import sys
 
 print(sys.version)
-print('nama is {0} age is {1}'.format('naga',27))
-print('nama is %s age is %d'%('naga',27))
-print('%.9f'%(123456789.123456789))
+print('nama is {0} age is {1}'.format('naga', 27))
+print('nama is %s age is %d' % ('naga', 27))
+i = 1234.2345
+print('{:.8f}'.format(i))
+print(f'{i:.7f}')
+print('%.6f'%i)
 
 name='naga'
 print(f'name is {name}')
-print(36%4)
-print(36//4)
+print(36 % 4)
+print(36 // 4)
 print(2*4)
